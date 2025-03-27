bebbd64f3736dffd9b052f1a92a6086ff7886e92
cmoulliard <cmoulliard@redhat.com>
2025-03-27 16:04:53 +0100

    Refactored the code to set as default value for the Dockerfile path: src/main/docker/Dockerfile.jvm and pass the prameter from the pipelinerun to the pipeline task's buildah as param for DOCKERFILE
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- common/src/main/java/io/quarkiverse/tekton/pipeline/BuildTestPushPipeline.java
+++ common/src/main/java/io/quarkiverse/tekton/pipeline/BuildTestPushPipeline.java
@@ -1,19 +1,13 @@
 package io.quarkiverse.tekton.pipeline;
 
 import io.fabric8.tekton.v1.Pipeline;
-import io.fabric8.tekton.v1.PipelineBuilder;
 import io.quarkiverse.tekton.common.utils.Resources;
 import io.quarkiverse.tekton.common.utils.Serialization;
-import io.quarkiverse.tekton.visitors.AddParamSpecDefaultValue;
 
 public class BuildTestPushPipeline {
 
     public static Pipeline create() {
         Pipeline pipeline = Serialization.unmarshal(Resources.read("/tekton/pipelines/build-test-push.yaml"));
-        pipeline = new PipelineBuilder(pipeline)
-                .accept(
-                        new AddParamSpecDefaultValue("DOCKERFILE", "src/main/docker/Dockerfile.jvm"))
-                .build();
         return pipeline;
     }
 }
--- common/src/main/java/io/quarkiverse/tekton/task/BuildahTask.java
+++ common/src/main/java/io/quarkiverse/tekton/task/BuildahTask.java
@@ -1,18 +1,13 @@
 package io.quarkiverse.tekton.task;
 
 import io.fabric8.tekton.v1.Task;
-import io.fabric8.tekton.v1.TaskBuilder;
 import io.quarkiverse.tekton.common.utils.Resources;
 import io.quarkiverse.tekton.common.utils.Serialization;
-import io.quarkiverse.tekton.visitors.AddParamSpecDefaultValue;
 
 public class BuildahTask {
 
     public static Task create() {
         Task task = Serialization.unmarshal(Resources.read("/tekton/tasks/buildah.yaml"));
-        task = new TaskBuilder(task)
-                .accept(new AddParamSpecDefaultValue("DOCKERFILE", "src/main/docker/Dockerfile.jvm"))
-                .build();
         return task;
     }
 }
